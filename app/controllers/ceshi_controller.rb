6bde1b058920d67bff5d77f040723149109b6cc3
shrek82 <37294812@qq.com>
2013-06-14 11:45:15 +0800

    lz edit
    lz edit

--- app/controllers/ceshi_controller.rb
+++
@@ -1,8 +0,0 @@
-class CeshiController < ApplicationController
-  def index
-    #require 'base64'
-    render :text => Common.activecode("37294812")
-    #render :text =>Base64.encode64('37294812@qq.com')
-    #render('users/_reg_input_mail',:layout => false)
-  end
-end
