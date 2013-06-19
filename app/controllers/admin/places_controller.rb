313b5db8e98a56f24e0f1f05972b3997fb1df43b
shrek82 <37294812@qq.com>
2013-06-19 18:25:19 +0800

    越来越好

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -1,6 +1,19 @@
+#coding: utf-8
 class Admin::PlacesController < AdminController
   def index
-    @places = Place.paginate(:page => params[:page], :per_page =>15)
+    @conditions=['id>10']
+    @places = Place.where(:id => 1..20).paginate(:page => params[:page], :per_page => 10)
+    keyword='植物园'
+    @places = Place.paginate(
+        :page => params[:page],
+        :per_page => 10,
+        :conditions => ["city_id = ? and name LIKE ?", 1, "%#{keyword}%"]
+    )
+
+  end
+
+  def new
+
   end
 
   def edit
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,9 @@ class AdminController < ApplicationController
   before_filter :manager_check
   private
   def manager_check
-
+    @benladeng='erqiu'
+    @request_media_type=request.media_type
+    @request_method=request.method
   end
 
 end
