bc5b704d301b03048c34a93398ab8b6e861e640f
shrek82 <37294812@qq.com>
2013-06-24 09:05:52 +0800

    添加字段

---
+++ app/controllers/common_controller.rb
@@ -0,0 +1,31 @@
+#coding: utf-8
+#常用的查询
+class CommonController < ApplicationController
+  #省份菜单
+  def provinces_select
+    @provinces=Province.select("id,name").all
+    respond_to do |format|
+      format.html { render :layout => false }
+      format.json { render json: @provinces }
+      format.xml { render xml: @provinces }
+    end
+  end
+
+  #县市下拉菜单
+  def cities_select
+    @cities=City.select("id,name").where(:province_id=>params[:province_id]).all
+    respond_to do |format|
+       format.html {render :layout => false}
+       format.json { render json: @cities }
+    end
+  end
+
+  #区下拉菜单
+  def areas_select
+    @areas=Area.select("id,name").where(:city_id=>params[:city_id]).all
+    respond_to do |format|
+      format.html {render :layout => false}
+      format.json { render json: @areas }
+    end
+  end
+end
