cf799f8493ace39bd6d29978531f0de5098532f6
shrek82 <37294812@qq.com>
2013-10-18 09:58:12 +0800

    增加技巧

--- study/module.js
+++ study/module.js
@@ -6,15 +6,76 @@ AMD模块
 CommonJS模块
 ECMAScript Harmony模块
  */
-var testModel = (function() {
+
+//testModel是一个Function
+var testModel = function() {
   var count = 0;
   return {
-    jiajia: function() {
+    fun1: function() {
       return count++;
     },
-    reset: function() {
+    fun2: function() {
       console.log("value is " + count);
     }
   }
-})
+};
+console.log(testModel);
+//实例化
+var myObj = testModel();
+myObj.fun2();
 
+/*
+[Function]
+value is 0
+ */
+
+console.log("---------------------------------");
+	
+
+// testModel依然是一个Function
+var testModel = (function() {
+  var count = 0;
+  return {
+    fun1: function() {
+      return count++;
+    },
+    fun2: function() {
+      console.log("value is " + count);
+    }
+  }
+});
+console.log(testModel);
+//实例化
+//
+var myObj = testModel();
+myObj.fun2();
+testModel().fun1();
+testModel().fun1();
+testModel().fun1();
+myObj.fun2();
+
+
+console.log("---------------------------------");
+
+// testModel是一个Hash形式的对象
+var testModel = (function() {
+  var count = 0;
+  return {
+    fun1: function(num) {
+      return count++;
+    },
+    fun2: function() {
+      console.log("value is " + count);
+    }
+  };
+})();
+
+//实例化,myObj只是testModel的一个引用,testmodel值变化,myObj也会变化
+console.log(testModel);
+var myObj = testModel;
+myObj.fun2();
+testModel.fun1();
+testModel.fun1();
+testModel.fun1();
+testModel.fun1();
+myObj.fun2();
---
+++ study/skill.js
@@ -0,0 +1,1 @@
+["123",'sdfsdf,'sdfsdf','sdfsdf'].joio(",")
