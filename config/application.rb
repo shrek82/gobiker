3d6494d66c15fe70d9b01b4c3c686f2706c96856
shrek82 <37294812@qq.com>
2013-12-27 14:44:31 +0800

    update

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,8 @@ module Gobiker
 
     config.i18n.default_locale = :cn
 
+    config.i18n.enforce_available_locales = true
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
