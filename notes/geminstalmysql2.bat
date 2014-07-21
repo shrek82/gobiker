6a30fd8568c4e53114ca1903a93a26509042cad0
shrek82 <37294812@qq.com>
2014-07-21 09:08:00 +0800

    增加rails 配置笔记

---
+++ notes/geminstalmysql2.bat
@@ -0,0 +1,1 @@
+gem install mysql2 -- '--with-mysql-include="C:\Program Files (x86)\MySQL\MySQL Connector C 6.0.2\include" --with-mysql-lib="C:\Program Files (x86)\MySQL\MySQL Connector C 6.0.2\lib\opt"'
